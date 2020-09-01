3955d468fdfd069a4856b72ac1bcc8a0b12f7305
Fedor Saevich <f.saevich@gmail.com>
2020-09-01 19:16:37 +0300

    * Clean HTML template

--- ItAcademy.SchoolAdmin.Web/App_Start/BundleConfig.cs
+++ ItAcademy.SchoolAdmin.Web/App_Start/BundleConfig.cs
@@ -7,13 +7,6 @@ namespace ItAcademy.SchoolAdmin.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle(Links.Bundles.Scripts.Bootstrap).Include(
-                      "~/Content/scripts/lib/bootstrap/bootstrap.js"));
-
-            bundles.Add(new StyleBundle(Links.Bundles.Content.Sitecontent).Include(
-                      "~/Content/css/bootstrap/bootstrap.css",
-                      "~/Content/css/site.css"));
-
             bundles.Add(new ScriptBundle(Links.Bundles.Scripts.JQuery).Include(
                         "~/Content/scripts/lib/jquery/jquery-{version}.js"));
 
